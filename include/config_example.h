5a327bcb0980bcfc9627a5b5a826b14ab8c1af7b
m0wmt <you@example.com>
2024-02-27 16:15:03 +0000

    Update example values

--- include/config_example.h
+++ include/config_example.h
@@ -4,5 +4,5 @@
 #define WIFI_PASSWORD "PASSWORD"
 #define MQTT_SERVER "11.12.13.14"
 #define MQTT_PORT 1234
-#define MQTT_USER "mqttuser"
-#define MQTT_USER_PASSWORD "mqttpassword"
+#define MQTT_USER "MQTTUSER"
+#define MQTT_USER_PASSWORD "MQTTPASSWORD"
