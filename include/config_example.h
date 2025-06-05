f30319c8194721cd23d449d6efd9c7f1783a484f
Bournetoride <68191539+Bournetoride@users.noreply.github.com>
2025-06-05 22:04:59 +0100

    Update config_example.h
    config

--- include/config_example.h
+++ include/config_example.h
@@ -2,8 +2,8 @@
 
 #define SSID "SSID"
 #define WIFI_PASSWORD "PASSWORD"
-#define MQTT_SERVER "11.12.13.14"
-#define MQTT_PORT 1234
+#define MQTT_SERVER "192.168.1.198"
+#define MQTT_PORT 1883
 #define MQTT_USER "MQTTUSER"
 #define MQTT_USER_PASSWORD "MQTTPASSWORD"
 #define SNTP_TIME_SERVER "pool.ntp.org"
